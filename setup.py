f7ecc3f9ca04a4c2b6846b948975910455d9068a
tryexceptpass <@tryexceptpass>
2018-04-26 00:24:33 -0400

    Updates to setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+from os import path
+
+
+# Get the long description from the README file
+with open(path.join('.', 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 
 setup(
     author = "tryexceptpass",
@@ -6,14 +13,18 @@ setup(
 
     name = "korv",
     version = "0.1.0",
-    description = "API framework over SSH",
-    long_description="",
+    description = "SSH API Frameowrk",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
 
     url = "https://github.com/tryexceptpass/korv",
 
     packages = find_packages(),
 
-    install_requires = [ 'asyncssh' ],
+    install_requires = ['asyncssh'],
+    python_requires='>=3.6',
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
 
     license = "MIT",
     classifiers = [ 'License :: OSI Approved :: MIT License',
@@ -24,10 +35,17 @@ setup(
                     'Framework :: AsyncIO',
 
                     'Programming Language :: Python :: 3 :: Only',
+                    'Programming Language :: Python :: 3.6',
 
                     'Development Status :: 4 - Beta',
                   ],
 
-    keywords = [ 'ssh', 'api', 'framework' ],
+    keywords = 'ssh api framework',
 
+    project_urls={
+        'Gitter Chat': 'https://gitter.im/try-except-pass/korv',
+        'Say Thanks!': 'https://saythanks.io/to/tryexceptpass',
+        'Source': 'https://github.com/tryexceptpass/korv',
+        # 'Documentation': 'http://korv.readthedocs.io/en/latest/',
+    },
 )
